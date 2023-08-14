83377a7451e4aed95e87ee1b71e73abd378e677c
Saurabh Kirve <>
2023-08-14 21:58:43 +0530

    !

--- subscribe.php
+++ subscribe.php
@@ -1,33 +1,58 @@
 <?php
-$email = $_POST['email'];
+// Validate and sanitize user input
+if (isset($_POST['email'])) {
+    $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
 
-// Database connection
-$servername = "localhost";
+    // Database connection
+  $servername = "localhost";
+$database = "u335992634_shreefurniture";
 $username = "u335992634_sanchan";
 $password = "@Sanchan77";
-$dbname = "u335992634_shreefurniture";
+ 
+// Create connection
+ 
+$conn = mysqli_connect($servername, $username, $password, $database);
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-// Check if email already exists
-$checkSql = "SELECT id FROM email_subscriptions WHERE email = '$email'";
-$result = $conn->query($checkSql);
-
-if ($result->num_rows > 0) {
-    echo "You have already subscribed!";
-} else {
-    // Insert email into database
-    $insertSql = "INSERT INTO email_subscriptions (email) VALUES ('$email')";
-
-    if ($conn->query($insertSql) === TRUE) {
-        echo "Subscribed successfully";
-    } else {
-        echo "Error: " . $insertSql . "<br>" . $conn->error;
+   
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
     }
-}
 
-$conn->close();
+    // Prepared statement
+    $checkSql = "SELECT id FROM email_subscriptions WHERE email = ?";
+    $stmt = $conn->prepare($checkSql);
+    $stmt->bind_param("s", $email);
+
+    // Execute the statement
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    // Check if email already exists
+    if ($result->num_rows > 0) {
+        echo "You have already subscribed!";
+    } else {
+        // Escape the email address
+        $email = mysqli_real_escape_string($conn, $email);
+
+        // Insert email into database
+        $insertSql = "INSERT INTO email_subscriptions (email) VALUES (?)";
+        $stmt = $conn->prepare($insertSql);
+        $stmt->bind_param("s", $email);
+
+        // Execute the statement
+        if ($stmt->execute()) {
+            echo "Subscribed successfully";
+        } else {
+            echo "Error: Subscription failed";
+        }
+    }
+
+    // Close the statement
+    $stmt->close();
+
+    // Close the connection
+    $conn->close();
+} else {
+    echo "Error: Invalid input";
+}
 ?>
